c9ab134da8ffbffe5117f635c25b5678d8b3e53c
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-01 17:45:41 +0530

    Update commandslist.js

--- commands/commandslist.js
+++ commands/commandslist.js
@@ -12,31 +12,31 @@ module.exports.run = (bot,message,args) => {
 
         case '1':
             sem.setTitle('Payments and Support!')
-            sem.addField('pd' , 'Creates a new paytm payment thread')
-            sem.addField('close' , 'Closes your payment thread')
+            sem.addField('!pd' , 'Creates a new paytm payment thread')
+            sem.addField('!close' , 'Closes your payment thread')
             break;
         case '2':
             sem.setTitle('Moderation')
-            sem.addField('warn @mention reason' , 'Warns mentioned user with given reason')
-            sem.addField('kick @mention reason' , 'Kicks mentioned user with given reason' )
-            sem.addField('ban @mention reason' , 'bans mentioned user with given reason')
-            sem.addField('mute @mention reason duration' , 'mutes mentioned user for given reason for specified duration of time')
-            sem.addField('unmute @mention reason' , 'unmutes muted user with reason for early unmute')
-            sem.addField('purge numberofmessages' , 'purges specified number of messages' )
+            sem.addField('!warn @mention reason' , 'Warns mentioned user with given reason')
+            sem.addField('!kick @mention reason' , 'Kicks mentioned user with given reason' )
+            sem.addField('!ban @mention reason' , 'bans mentioned user with given reason')
+            sem.addField('!mute @mention reason duration' , 'mutes mentioned user for given reason for specified duration of time')
+            sem.addField('!unmute @mention reason' , 'unmutes muted user with reason for early unmute')
+            sem.addField('!purge numberofmessages' , 'purges specified number of messages' )
             break;
         case '3':
             sem.setTitle('Miscellanious Commands')
-            sem.addField('+send @mention message' , 'sends message to mentioned user')
-            sem.addField('+say message' , 'says message')  
-            sem.addField('+members' , 'gives number of server members')
-            sem.addField('+dev' , 'gives info about me')
+            sem.addField('!send @mention message' , 'sends message to mentioned user')
+            sem.addField('!say message' , 'says message')  
+            sem.addField('!members' , 'gives number of server members')
+           
 
         default:
             
             sem.setTitle('Select a command category!')
         sem.addField('Payment and Support(1)' , 'Payment and Support commands')
         sem.addField('Moderation(2)' , 'Commands exclusive to mos')
-        sem.addField('Miscellanious(3)' , 'Misc commands')
+        sem.addField('Miscellaneous(3)' , 'Misc commands')
         sem.setDescription('Use ur command like this: +commands 2')
     }
     message.channel.send(sem)
