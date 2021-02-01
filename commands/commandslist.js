8002471dfd35fdea8bfa34a1584f83558162192e
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-01 17:31:13 +0530

    Update commandslist.js

--- commands/commandslist.js
+++ commands/commandslist.js
@@ -24,33 +24,7 @@ module.exports.run = (bot,message,args) => {
             sem.addField('unmute @mention reason' , 'unmutes muted user with reason for early unmute')
             sem.addField('purge numberofmessages' , 'purges specified number of messages' )
             break;
-        case '4':
-            sem.setTitle('Fun Commands')
-            sem.addField('meme <subreddit>' , 'Gets memes from a valid non nsfw mentioned subreddit')   
-            sem.addField('executes @mention' , 'executes mentioned user')
-            sem.addField('pp @mention' , 'Purchase fun pack @129 INR to activate!')
-            sem.addField('howgae @mention' , 'Purchase fun pack @129 INR to activate!')    
-            sem.addField('cmm <text>' , 'Purchase fun pack @129 INR to activate!')
-            sem.addField('trigger @mention' , 'Purchase fun pack @129 INR to activate!')
-            sem.addField('pat @mention' , 'Purchase fun pack @129 INR to activate!')  
-            break;
         case '3':
-            sem.setTitle('Economy')
-            sem.addField('Regular Rewards:' , 'Purchase Economy pack @249 INR to activate!')
-            sem.addField('+daily' , 'Purchase Economy pack @249 INR to activate!')
-            sem.addField('+weekly' , 'Purchase Economy pack @249 INR to activate!')
-            sem.addField('+bal' , 'Purchase Economy pack @249 INR to activate!')
-            sem.addField('+lb pg no.' , 'Purchase Economy pack @249 INR to activate!')
-            sem.addField('+work' , 'Purchase Economy pack @249 INR to activate!')
-            sem.addField('+beg' ,'Purchase Economy pack @249 INR to activate!')
-            sem.addField('+give @mention amount' , 'Purchase Economy pack @249 INR to activate!')
-             break;
-        case '5':
-            sem.setTitle('Game.tv commands')
-            sem.setDescription('Under Development!')
-            break;
-
-        case '6':
             sem.setTitle('Miscellanious Commands')
             sem.addField('+send @mention message' , 'sends message to mentioned user')
             sem.addField('+say message' , 'says message')  
