5edde11e113aa640ab0c1e87f15d62ca5bd6c7f4
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-01 20:25:08 +0530

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,76 +1,49 @@
 
-module.exports.run = async (bot,message,args) => {
-  const ms = require('ms')
-  const Discord = require('discord.js')
-    if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("U cannot mute someone u idiot!")
-
-      
-      let person = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]))
-      if(!person) return message.reply("Oye YE kaun hai?Thikse mention kar kisiko!")
-
-      let mainrole = message.guild.roles.cache.find(role => role.name === "⊱ |──►Verified◄──| ⊰")
-      let muterole = message.guild.roles.cache.find(role => role.name === "Muted")
-
-      if(!muterole) return message.reply("YE MUTE ROLE KAHA GAYA!!!!")
 
 
-      let time = args[1];
-      let reason = args[2];
 
-      if(!reason) return message.reply("Kyu mute kar raha hai bechaare ko?Give reason!!!")
-
-      
-
-      if(!time){
-          return message.reply("Kab tak mute karega is darinde ko??Koi Time to bol!")
-      }
-
-      person.roles.remove(mainrole.id);
-      person.roles.add(muterole.id);
-
-      message.channel.send(`**${person.user.username}** has been muted for ${ms(ms(time))}`);
-      mutedst = new Discord.MessageEmbed()
-         .setColor('#FF0000')
-         .setTitle('Darinda Muted!')
-         .addField('Darinda Name:' , person.user.username , true)
-         .addField('Muted by:', message.author.username , true)
-         .addField('Mute Duration' , time , true)
-         .addField('Reason' , reason , true)
-         .setTimestamp()
-         .setFooter("Dev: RLX | BlazingDragon" , 'https://i.imgur.com/5O2LozU.jpg')
-        message.channel.send(mutedst)  
-        bot.channels.cache.get('778559682712961031').send(mutedst)
-
-
-      setTimeout(function(){
-
-          if(!person.roles.cache.has(muterole.id)){
-    
-            message.channel.send(`Tried to unmute **${person.user.username}** but he was already unmuted!`)
-            return
-            
             
             }
          
           person.roles.add(mainrole.id);
-          person.roles.remove(muterole.id);
-          unmuted = new Discord.MessageEmbed()
-            .setColor("#FF4D00")
-            .setTitle("User Unmuted!")
-            .addField("User:" , person.user.username , true)
-            .addField('Was Muted by:', message.author.username , true)
-            .addField('Reason for mute' , reason , true)
-            .addField("Unmuted After:" , time , true)
-            .setTimestamp()
-            .setFooter("Dev: RLX | BlazingDragon" , 'https://i.imgur.com/5O2LozU.jpg')
-        message.channel.send(unmuted)  
-        bot.channels.cache.get('778559682712961031').send(unmuted)
-
-
+          BlazingDragon" , 'https://i.imgur.com/5O2LozU.jpg')
+        
       },ms(time));
 }
 
 module.exports.help = {
     name: "mute",
     aliases: ["chup" , "Mute"]
+const ms = require('ms');
+module.exports = {
+    name: 'mute',
+    description: "This mutes a member",
+    execute(message, args) {
+        const target = message.mentions.users.first();
+        if (target) {
+ 
+            let mainRole = message.guild.roles.cache.find(role => role.name === 'member');
+            let muteRole = message.guild.roles.cache.find(role => role.name === 'mute');
+ 
+            let memberTarget = message.guild.members.cache.get(target.id);
+ 
+            if (!args[1]) {
+                memberTarget.roles.remove(mainRole.id);
+                memberTarget.roles.add(muteRole.id);
+                message.channel.send(`<@${memberTarget.user.id}> has been muted`);
+                return
+            }
+            memberTarget.roles.remove(mainRole.id);
+            memberTarget.roles.add(muteRole.id);
+            message.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms(ms(args[1]))}`);
+ 
+            setTimeout(function () {
+                memberTarget.roles.remove(muteRole.id);
+                memberTarget.roles.add(mainRole.id);
+            }, ms(args[1]));
+        } else {
+            message.channel.send('Cant find that member!');
+        }
+    }
 }
+ 
