e5c9fd52c7c6be0f80cca4d32c6c581f28b3dc0c
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-01 20:34:37 +0530

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,46 +1,42 @@
-module.exports.run = async (bot,message,args) => {
-    const Discord = require('discord.js')
-    const ms = require('ms')
-    if(!message.member.hasPermission('KICK_MEMBERS')) return message.reply('Koi zaroorat nahi beta!')
-          
-      
-      let person = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[1]))
-      if(!person) return message.reply("Oye YE kaun hai?Thikse mention kar kisiko!")
+module.exports = {
 
-      let mainrole = message.guild.roles.cache.find(role => role.name === "⊱ |──►Verified◄──| ⊰")
-      let muterole = message.guild.roles.cache.find(role => role.name === "Muted")
-      console.log(person.roles.cache.map(role => role.name))
-      
-       
+    name: 'unmute',
 
-     if(!person.roles.cache.has(muterole.id)){
-    
-            message.channel.send('He is already unmuted!')
-            return
-            
-            
-            }
+    description: "This unmutes a member",
 
-        let ureason =  args[1]
-        if(!ureason) return message.reply("Darinde ko kyu unmute kar raha hai?GIVE Reason like this: +unmute @mention reason") 
-      
-      person.roles.remove(muterole.id)
-      person.roles.add(mainrole.id)
-      message.channel.send("User Unmuted before mute duration was completed.")
-      unmutedforetime = new Discord.MessageEmbed()
-            .setColor("#FF4D00")
-            .setTitle("User Unmuted!")
-            .addField("User:" , person.user.username , true)
-            .addField('Unmuted by:', message.author.username , true)
-            .addField('Reason:'  , ureason , true)
-            .setTimestamp()
-            .setFooter("Dev: RLX | BlazingDragon" , 'https://i.imgur.com/5O2LozU.jpg')
-        message.channel.send(unmutedforetime)  
-        bot.channels.cache.get('727772378268172340').send(unmutedforetime)
+    execute(message, args){
+
+        const target = message.mentions.users.first();
+
+        if(target){
+
+            let mainRole = message.guild.roles.cache.find(role => role.name === 'member');
+
+            let muteRole = message.guild.roles.cache.find(role => role.name === 'mute');
+
+ 
+
+            let memberTarget= message.guild.members.cache.get(target.id);
+
+ 
+
+            memberTarget.roles.remove(muteRole.id);
+
+            memberTarget.roles.add(mainRole.id);
+
+            message.channel.send(`<@${memberTarget.user.id}> has been unmuted`);
+
+        } else{
+
+            message.channel.send('Cant find that member!');
+
+        }
+
+    }
 
 }
 
-module.exports.help = {
-    name: "unmute",
-    aliases: ["Unmute"]
-}
+
+
+
+
