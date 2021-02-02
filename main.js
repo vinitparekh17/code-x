6707088c6c71f530ab54fef11b60494148b7b2be
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-02 10:00:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ bot.on("ready", async () => {
     setInterval(function() {
         numberomember =`${bot.users.cache.size}`
 
-        let activities = [ '$VINIT$' ,  '+pd for Payment | +help for list of commands']
+        let activities = [ 'WITH $VINIT$' ,  '!ticket for Payment | !help for list of commands']
 
        let activity = activities[Math.floor(Math.random()*activities.length)]
 
