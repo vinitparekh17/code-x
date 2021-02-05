2bce6f294353f61cb348a3d7ce0e0c7404dcda5f
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-05 21:32:29 +0530

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,5 +1,5 @@
 module.exports.run = async(bot,message,args) => {
-var result = message.split(" ")
+var result = message.split(" ").slice(1).join(" ")
         let evaled = eval(result);
         const.log(result)
         message.chennal.send(evaled)
