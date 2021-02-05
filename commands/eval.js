b9f568755154724201142930c3dc9689d70a9da2
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-05 21:36:00 +0530

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,4 +1,5 @@
 module.exports.run = async(bot,message,args) => {
+const.requried = ("discord.js")        
 var result = message.split(" ").slice(1).join(" ")
         let evaled = eval(result);
         const.log(result)
