71dc142446d158e309202497bcedb6eb32667776
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-05 16:44:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ bot.user.setStatus('dnd')
     setInterval(function() {
         numberomember =`${bot.users.cache.size}`
 
-        let activities = [ 'Champions' , ` ${bot.guild.cache.size} servers! ` ,  '!ticket for Payment | !help for list of commands']
+        let activities = [ ` ${bot.guild.cache.size} servers! ` , '!ticket for Payment | !help for list of commands']
 
        let activity = activities[Math.floor(Math.random()*activities.length)]
 
