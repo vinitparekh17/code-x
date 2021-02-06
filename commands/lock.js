08ef6ddb7dec2481b7218e4c03008eab8cba97fa
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-06 11:49:30 +0530

    Update lock.js

--- commands/lock.js
+++ commands/lock.js
@@ -4,10 +4,12 @@ const Discord = require('discord.js')
 module.exports.run = async(bot,message,args) => {
  if(!message.member.haspermissiom(MANAGE_CHANNELS)) return message.reply("You don't have enough permission to use this command!")
   
-message.channel.overwritePermission([{
+message.channel.overwritePermission: [
+ {
    id: message.guild.id,
    deny: ['SEND_MESSAGES'],
-  },],);
+  },
+ ]
   message.channel.send(`${message.chennal} has been locked`)
   }
 
