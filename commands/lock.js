6c7c62194e5850b97820407f236eca65a2ffd3f0
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-07 21:10:05 +0530

    lock.js

--- commands/lock.js
+++ commands/lock.js
@@ -11,8 +11,7 @@ module.exports.run = async (bot,message,args) => {
   await lockChannel.updateOverwrite(role, {
     SEND_MESSAGES: false
   }) .catch(err => console.log(err));
-  message.send => {
-  let lockembed = new Discord.MessgeEmbed()
+  const lockembed = new Discord.MessgeEmbed()
   .setColor('#00FF00')
   .setDiscription(`<a:ac_loading:806430327924457492> <#${message.channel.id}> has been locked!`)
   message.channel.send(lockembed)
