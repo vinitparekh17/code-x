d4debbd0420d23ee2cf7e2b1227a014ffd9bfd75
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-07 12:09:08 +0530

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -8,8 +8,8 @@ module.exports.run = async (bot,message,args) =>{
         message.channel.send("Please specify a number between 2 and 100");
     if(!Number.isInteger(delength)) return message.channel.send("This is not a valid number!");
     if(isNaN(delenghth)) return message.channel.send("Plz, provide valid number")
-           return
-}
+    return
+        }
 
 await message.channel.bulkDelete(delength)
 .then(messages => console.log(`Bulk deleted ${messages.size} messages`))
