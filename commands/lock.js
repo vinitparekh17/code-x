91f3f79c69fde62ef41ac17e8cff72581c31c6d3
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-07 20:41:11 +0530

    lock.js

--- commands/lock.js
+++ commands/lock.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot,message,args) => {
     SEND_MESSAGES: false
   }) .catch(err => console.log(err));
   let lockembed = new Discord.MessgeEmbed()
-  .setTitle:()
+  .setTitle:(' ')
   .setColor:('#00FF00')
   .setDiscription:(`<a:ac_loading:806430327924457492> <#${message.channel.id}> has been locked!`)
   message.channel.send(lockembed)
