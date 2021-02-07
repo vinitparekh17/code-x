f8bcf3d65cb1d96eedc848537cd18766d8292456
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-07 17:59:59 +0530

    Update lock.js

--- commands/lock.js
+++ commands/lock.js
@@ -12,9 +12,8 @@ module.exports.run = async (bot,message,args) => {
     SEND_MESSAGES: deny
   }) .catch(err => console.log(err));
   message.channel.send('Channel has been **Locked**')
-      }
-
-    }
+      
+}
 
 module.exports.help = {
     name: "lock",
