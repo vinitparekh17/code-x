db01da416b1e82702ab679da7fe35ef23cdea124
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-07 19:28:47 +0530

    Update lock.js

--- commands/lock.js
+++ commands/lock.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot,message,args) => {
   const ms = require('ms')
   const Discord = require('discord.js')
-  if(!message.member.haspermission("MANAGE_CHANNELS")) return message.channel.send('You do not have enough permission');
+  if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send('You do not have enough permission');
   if(!message.guild.me.haspermission("MANAGE_CHANNELS")) return message.channel.send('Missing **"MANAGE CHANNEL"** permission');
   
   const role = message.guild.cache.get('794899914437951488');
