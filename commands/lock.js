eda1b31cdc1d1d80d07ba3e29ae34c109a1f1aaf
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-07 22:01:37 +0530

    lock.js

--- commands/lock.js
+++ commands/lock.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot,message,args) => {
   if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send('You do not have enough permission');
   if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send('Missing **"MANAGE CHANNEL"** permission');
   
-  const role = message.guild.roles.cache.get('794899914437951488' || '771731302772113408`);
+  const role = message.guild.roles.cache.get('794899914437951488');
   let lockChannel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
   if(!lockChannel) lockChannel = message.channel;
   
