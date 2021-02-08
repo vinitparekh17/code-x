466be92dc95115018bfdf2cdc34b3b10dbfa610d
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-08 22:48:29 +0530

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -5,12 +5,12 @@ const Discord = require('discord.js')
 let mentionedMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 if (!mentionedMember) mentionedMember = message.member;
 
-const avbd = Discord.messageEmbed(){
+const avbd = Discord.messageEmbed()
 .setTitle(mentionedMember.user.tag + "'s Avatar!")
 .setImage(mentionedMember.usar.displayAvatarURL());
 
   message.channel.send(avbd);
- }
+ 
 }
 
 module.exports.help = {
