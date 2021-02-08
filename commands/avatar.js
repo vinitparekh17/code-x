1f72476b5803412339cb352e267d2568ef19fdbf
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-08 22:16:24 +0530

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,17 @@
+module.exports.run = async (bot,message,args) => {
+let mentionedMember = message.mentiones.members.first() || message.guild.members.cache.get(args[0]);
+if (!mentionedMember) = mentionedMember = message.member;
+
+async run(client, message, args) {
+const avbd = Discord.messageEmbed()
+.setTitle(mentionedMember.user.tag + "'s Avatar!")
+.setImage(mentionedMember.usar.displayAvatarURL());
+
+  message.channel.send(avbd);
+ }
+}
+module.exports.help = {
+name: "avatar",
+aliases: ["av"]
+
+}
