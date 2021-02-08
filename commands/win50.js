eb4050694eab1a19f63f3aab951f37e65fdc825e
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-08 15:53:55 +0530

    Update win50.js

--- commands/win50.js
+++ commands/win50.js
@@ -3,6 +3,7 @@ const ms = require ('ms')
 module.exports.run = async (bot,message,args)=>{
   if(!message.member.hasPermission("ADMINISTRATION")) return message.channel.send('Teri aukat nhi h ye command use karne ki');
   if(args[0] == null) message.channel.send("Spacify someone as a winner");
+const message.content.slice(prefix.length).trim().split(' ')
 
 message.channel.send(` **Aqua Champion Solo SD #{args[5]} **
 
