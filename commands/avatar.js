5e5bd72f4e9fab993742043fff833907956d0143
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-08 23:19:01 +0530

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -8,7 +8,7 @@ if (!mentionedMember) mentionedMember = message.member;
 const avbd = new Discord.MessageEmbed()
 .setTitle(mentionedMember.user.tag + "'s Avatar!")
 .setColor("RANDOM")
-.setImage(mentionedMember.user.displayAvatarURL({dynamic:true}));
+.setImage(mentionedMember.user.displayAvatarURL({dynamic: true}));
 
   message.channel.send(avbd);
  
