eb812d0a3b3c3ad8e8b480785f01387b94a6d7b8
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-08 15:46:47 +0530

    Update win50.js

--- commands/win50.js
+++ commands/win50.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js')
-const ms = require ('ms)
+const ms = require ('ms')
 module.exports.run = async (bot,message,args)=>{
   if(!message.member.hasPermission("ADMINISTRATION")) return message.channel.send('Teri aukat nhi h ye command use karne ki');
+  if(args[0] == null) message.channel.send("Spacify someone as a winner");
 
 await message.channel.send(' **Aqua Champion Solo SD #{args[5]} **
 
