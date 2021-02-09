53c18912fef7138e6b70e0fcadddbefdc46a2495
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-09 12:48:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -78,6 +78,25 @@ bot.on ("message"  , async message  =>  {
 
     const server = message.guild;
 
+client.on("message", message => {
+  const args = message.content.split(" ").slice(1);
+
+  if (message.content.startsWith(config.prefix + "eval")) {
+    if(message.author.id !== config.ownerID) return;
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+
+      message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+  }
+});
+
 
       if (message.channel.type === 'dm') {
 
