35f4486f9d9a4d0096ddd4244988d122646912e3
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-09 19:11:27 +0530

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -5,6 +5,8 @@ module.exports.run = async (bot,message,args)=>{
         message.channel.send("Kick to hoga tu <:SaaleNoob:717419994820116530>")
         return
 let member = message.mentions.members.first();
+member.kick;
+message.channel.send(`${member.username} has been kicked!`}
 
 }
 
