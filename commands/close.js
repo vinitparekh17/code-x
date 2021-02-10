0b633c8a930b27da96d704f6d0ff69bfa52ac703
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-10 15:37:41 +0530

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -1,7 +1,7 @@
 module.exports.run = async(bot,message,args) => {
     if(message.channel.name.startsWith('pay','closed','gpgc') || message.channel.name.startsWith('Closed')){
 message.channel.send('Deleting channel in 5 seconds...')
-        .setTimeout({message.channel.delete()}, x.value = 5 seconds, 5000}
+       message.channel.delete()
     }
     else{message.channel.send("This is not ticket channel Noob!")}
 
