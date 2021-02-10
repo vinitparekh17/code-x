f8c024bb8a53644ab744ef80ab30993a63d3f3fd
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-10 11:36:15 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,7 @@
 module.exports.run = async (bot,message,args) => {
     const m = await message.channel.send("Ping?")
-const pingbd = new Discord.MessageEmbed()
-.setTitle('Ping?')
-.setColor('#FFF500')
-.setDescription(`Pong! ${m.createdTimestamp - message.createdTimestamp}ms`)
-    message.channel.edit('pingbd')
+m.edit(`Pong! ${m.createdTimestamp - message.createdTimestamp}ms`)
+
 }
 
 module.exports.help = {
