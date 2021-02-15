e5dc75db4302e8709e05c2e5b1d46ee23af82c52
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-15 20:36:51 +0530

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const fetch = require('node-fetch');
 module.exports.run = async (bot,message,args) =>{
 fetch('https://meme-api.herokuapp.com/gimme')
 .then(res => res.json())
