95efbfa39335676125a702cea9d00fc391784080
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-16 22:51:16 +0530

    Update win.js

--- commands/win.js
+++ commands/win.js
@@ -6,7 +6,7 @@ if (!a) return message.channel.send('Please give winners ');
 const embed = new Discord.MessageEmbed()
 
 .setTitle("Each winner getss 50rs each")
-.addField('<a:redBadge:807268819000492073>Winners<a:ArrowRight:807268621516013598>', a)
+.addField('<a:Minigame_Crown:795920833645903872> Winners <a:Minigame_Crown:795920833645903872>', a)
 
 .setColor("#00ffff")   
 .setFooter(message.client.user.username, message.client.user.displayAvatarURL());
