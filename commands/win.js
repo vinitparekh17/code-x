c03a58982833c27d75efaf12209a9adaf7b5e615
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-16 19:39:34 +0530

    Create win.js

---
+++ commands/win.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+module.exports.run = async (bot,message,args) =>{
+const a = args.join(" ")
+if (!a) return message.channel.send('Please give winners ');
+
+const embed = new Discord.MessageEmbed()
+
+.setTitle("Each winner getss 50rs each")
+.addField('<a:redBadge:807268819000492073>Winners<a:ArrowRight:807268621516013598>', a)
+
+.setColor("#00ffff")   
+.setFooter(message.client.user.username, message.client.user.displayAvatarURL());
+message.channel.send(embed), message.react('✅');
+
+  }
+
+module.exports.help = {
+    name: "win",
+    aliases: ["win"]
+}
