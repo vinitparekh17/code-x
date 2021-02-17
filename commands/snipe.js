da6c9c9a8ba1cd858ca4595dc1e937a577d49fd0
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-17 18:07:18 +0530

    Update snipe.js

--- commands/snipe.js
+++ commands/snipe.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 module.exports.run = async(bot,message,args) => {
 
-const msg = bot.snipes.get(message.channel.id);
+const msg = client.snipes.get(message.channel.id);
 if (!msg) return message.channel.send('No deleted message found!');
 
 const embed = new Discord.MessageEmbed()
