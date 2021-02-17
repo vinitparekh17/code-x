1454080baa0ba07fccaeff9ffa53c331afcd1034
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-17 17:58:23 +0530

    Create snipe.js

---
+++ commands/snipe.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+module.exports.run = async(bot,message,args) => {
+
+const msg = client.snipes.get(message.channel.id);
+if (!msg) return message.channel.send('No deleted message found!');
+
+const embed = new Discord.MessageEmbed()
+.setAuthor(message.author.tag, message.author.displayAvatarURL())
+.setDescription(msg.content)
+.setTimestamp()
+.setColor('RANDOM')
+
+message.channel.send(embed);
+ }
+}
+
+module.exports.help = {
+    name: "snipe",
+    aliases: ["snipe"]
+}
