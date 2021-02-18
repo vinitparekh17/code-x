c9c23d152ae96f0ed7cbaa0ba598de169b0df1b9
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-18 20:38:39 +0530

    Create embed.js

---
+++ commands/embed.js
@@ -0,0 +1,38 @@
+ const Discord = require('discord.js');
+ const ms = require('ms');
+module.exports.run = async(client, message, args) => {
+        if (!message.member.permissions.has("MANAGE_GUILD")) return message.channel.send({ embed: { color: "RED", description: `⚠  ${message.author} You are missing the following permission: \`MANAGE SERVER\`` } });
+
+        const Channel = await message.mentions.channels.first();
+
+        if (!Channel) return message.channel.send({ embed: { color: "ORANGE", description: `**Required Arguments** \n \`\`\`create-embed <#channel> <embed.color>\`\`\`` } });
+
+        const emebdcolor = args[1];
+        if (!emebdcolor) return message.channel.send({ embed: { color: "ORANGE", description: `**Required Arguments** \n \`\`\`create-embed <#channel> <embed.color>\`\`\`` } });
+
+        const embed = new Discord.MessageEmbed()
+        embed.setColor(`${emebdcolor}`)
+
+        if (Channel) {
+            message.channel.send(`${message.author} - Please type below the message. (time: 100s)`)
+                .then(function () {
+                    message.channel.awaitMessages(m => m.author.id == message.author.id, {
+                        max: 1,
+                        time: 1000 * 100,
+                        errors: ['time'],
+                    })
+                        .then((collected) => {
+                            embed.setTitle(`${collected.first()}`)
+                            Channel.send(embed).catch(err => message.channel.send(`⚠ **Error:** ${err}`))
+                        })
+                })
+                .catch(function () {
+                    message.channel.send(`⚠ ${message.author} - You didnt write down the message on time`);
+                });
+        }
+    }
+
+module.exports.help = {
+    name: "embed",
+    aliases: ["embed"]
+}
