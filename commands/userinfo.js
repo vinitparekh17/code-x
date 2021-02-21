30970e21c8345307eb29c25c0f110b0b3942aaa6
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-21 12:57:16 +0530

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -9,6 +9,8 @@ module.exports.run = async(bot, message, args) => {
 
     const embed = new Discord.MessageEmbed()
       .setAuthor(`User info for ${user.username}`, user.displayAvatarURL())
+      .setColor('RANDOM')
+      .setThumbnail(`user.displayAvatarURL()`)
       .addFields(
         {
           name: 'User tag',
