68e5584c00c172c222e145eb2986296d57abab26
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-22 23:45:34 +0530

    Update lock.js

--- commands/lock.js
+++ commands/lock.js
@@ -3,14 +3,12 @@ module.exports.run = async (bot,message,args) => {
   const Discord = require('discord.js')
   if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send('You do not have enough permission');
   if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send('Missing **"MANAGE CHANNEL"** permission');
-  
-  const role = message.guild.roles.cache.get('794899914437951488');
-  let lockChannel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
-  if(!lockChannel) lockChannel = message.channel;
-  
-  await lockChannel.updateOverwrite(role, {
-    SEND_MESSAGES: false
-  }) .catch(err => console.log(err));
+   message.channel.overwritePermissions([
+     {
+        id: message.guild.id,
+        deny : ['SEND_MESSAGES'],
+     },
+    ],);
   const lockembed = new Discord.MessageEmbed()
   .setColor('#00FF00')
   .setDescription(`<a:GG_true3:758699876858462258> <#${message.channel.id}> has been locked!`)
