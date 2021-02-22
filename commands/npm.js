1d2c2ae5aeb21d05b433ec95c763a6933c7eba14
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-22 23:38:27 +0530

    Create npm.js

---
+++ commands/npm.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const process = require('child_process');
+module.exports.run = async(bot, message, args) {
+          if(message.author.id === 'OWNER_ID') {
+const msg = await message.channel.send(`Please wait, this may take a white.`);
+msg.delete({timeout: 4000});
+process.exec(args.join(" ") , (error, stdout) => { let result = (stdout || error);
+message.channel.send(result, { code: "asciidoc", split: "\n"}).catch(err => message.channel.send(err))
+}) 
+} else { 
+return message.reply(`Developers Only !`); 
+}
+
+}
+
+module.exports.help = {
+    name: "non",
+    aliases: ["npm"]
+}
