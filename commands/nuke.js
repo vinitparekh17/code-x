6056e9aa6316217ddd69cf720dd27ce4bcbe0188
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-23 20:26:10 +0530

    Update nuke.js

--- commands/nuke.js
+++ commands/nuke.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 module.exports.run = async(bot, message, args) => {
 if message.member.hasPermission('MANAGE_CHANNELS') return message.reply('You don't have enough permission to use this command!');
 
