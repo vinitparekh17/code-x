7f46641192e4a44998e40210c9d2114b1d7fdac0
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-23 20:24:21 +0530

    Create nuke.js

---
+++ commands/nuke.js
@@ -0,0 +1,16 @@
+module.exports.run = async(bot, message, args) => {
+if message.member.hasPermission('MANAGE_CHANNELS') return message.reply('You don't have enough permission to use this command!');
+
+message.channel.clone().then((ch) => {
+    ch.setParent(message.channel.parent.id);
+    ch.setPosition(message.channsl.position);
+    message.channel.delete();
+
+ch.send('This channel has been nuked! https://c.tenor.com/SzfO_CqZSRwAAAAM/chicken-chicken-bro.gif ')
+   })
+}
+
+module.exports.help = {
+    name: "nuke",
+    aliases: ["nuke"]
+}
