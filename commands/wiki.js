299df35e9f03ad44ba7c2de422a3661f1fc095e6
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-23 13:09:50 +0530

    Update wiki.js

--- commands/wiki.js
+++ commands/wiki.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch') // npm i node-fetch
 module.exports.run = async(bot, message, args) => { // Update To Your Handler
 
         const wiki = args.join(" ");
-        if(!wiki) return message.reply('Provide A Query To Search.') // If No Topic Provided To Searched
+        if(args[0] == Null) return message.reply('Provide A Query To Search.') // If No Topic Provided To Searched
         const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(wiki)}` // From Here BOT Will Search For Searched Topic
 
         let response
