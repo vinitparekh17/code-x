0d84013c78f96e6bd1783170b08ccd9d6d93dc1d
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-23 10:43:39 +0530

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,31 +1,69 @@
-const Discord = require('discord.js');
-const ms = require('ms');
-module.exports.run = async(bot, message, args) => {
-let embed = new Discord.MessageEmbed()
+const { CustomEmbed, setCooldown } = require('../../utils/utils');
+const moment = require("moment");
 
-    .setTimestamp()
-    .setTitle("**Server Information**")
-    .setColor('ff0000')
-    .setThumbnail(message.guild.iconURL({ dynamic: true }))
-    .addField(`🎫 Server Name:`, message.guild.name, true)
-    .addField(`🆔 Server ID:`, message.guild.id, true)
-    .addField(`📎 Server Icon:`)
-    .setDescription(`[click here](message.guild.iconURL({format: 'png'}))`)
-    .addField(`👑 Server Owner:`, message.guild.owner, true)  
-    .addField(`🗺️ Server Religion`, message.guild.region, true)
-    .addField(`👥 Members Count:`, message.guild.members.cache.size, true)
-    .addField(`🤖 Bot Count:`, message.guild.members.cache.filter(member => member.user.bot).size, true)
-    .addField(`🚶 Humans:`, message.guild.members.cache.filter(member => !member.user.bot).size, true)
-    .addField(`😗 Emojis:`, message.guild.emojis.cache.size, true)
-    .addField(`👻 Animated Emojis:`,message.guild.emojis.cache.filter(emoji => emoji.animated).size,true )
-    .addField(`💬 Text Channels:`, message.guild.channels.cache.filter(channel => channel.type === 'text').size, true)
-    .addField(`🎤 Voice Channel:`, message.guild.channels.cache.filter(channel => channel.type === 'voice').size, true)
-    .addField(`👔 Roles Count:`, message.guild.roles.cache.size, true)
-    .setAuthor(message.author.username, message.author.displayAvatarURL({format: 'png'}))
-		message.channel.send(embed);
-  }
+const region = {
+    "brazil": "Brazil :flag_br:",
+    "europe": "Europe :flag_eu:",
+    "india": "India :flag_in:",
+    "japan": "Japan :flag_jp:",
+    "singapore": "Singapore :flag_sg:",
+    "us-central": "US-Central :flag_us:",
+    "us-east": "US-East :flag_us:",
+    "us-south": "US-South :flag_us:",
+    "us-west": "US-West :flag_us:",
+    "sydney": "Sydney :flag_au:",
+    "hongkong": "Hong Kong :flag_hk:",
+    "russia": "Russia :flag_ru:",
+    "southafrica": "South Africa :flag_za:"
+};
+
+module.exports.run = async function({client, message}) {
+        setCooldown(client, this, message)
+
+        const titleCase = str => {
+            return str.toLowerCase().replace(/_/g, " ").split(" ")
+                      .map(word => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+                      .join(" ")
+        }
+
+        let members = message.guild.members.cache.filter(member => !member.user.bot).size
+        let onlineMembers = message.guild.members.cache.filter(member => !member.user.bot).filter(member => member.presence.status !== "offline").size
+        let bots = message.guild.members.cache.filter(member => member.user.bot).size
+        let onlineBots = message.guild.members.cache.filter(member => member.user.bot).filter(member => member.presence.status !== "offline").size
+        let textChannels = message.guild.channels.cache.filter((c) => c.type === "text").size;
+        let voiceChannels = message.guild.channels.cache.filter((c) => c.type === "voice").size;
+        let categories = message.guild.channels.cache.filter((c) => c.type == "category").size;
+        let roleCount = message.guild.roles.cache.size - 1;
+
+        let icon = message.guild.iconURL({ dynamic: true, size: 512 })
+        if(!icon) { icon = "https://i.imgur.com/AWGDmiu.png"}
+
+        const embed = new CustomEmbed({ client: client})
+            .setAuthor(`${message.guild.name}`, icon)
+            .setDescription(`${message.guild.name} was created on ${moment(message.guild.createdAt).format("MMM DD YYYY")}`)
+            .setThumbnail(icon)
+            .addFields(
+                { name: "Region", value: `${region[message.guild.region]}`, inline:true },
+                { name: "Total Users/Bots", value: `${message.guild.members.cache.size} Users/Bots`, inline: true },
+                { name: "Users", value: `${members} Users (${onlineMembers} Online)`, inline: true },
+                { name: "Bots", value: `${bots} Bots (${onlineBots} Online)`, inline: true },
+                { name: "Boosts", value: `${message.guild.premiumSubscriptionCount} Boosts (Tier ${message.guild.premiumTier})`, inline: true },
+                { name: "Text Channels", value: `${textChannels}`, inline: true },
+                { name: "Voice Channels", value: `${voiceChannels}`, inline: true },
+                { name: "Categories", value: `${categories}`, inline: true },
+                { name: "Verification Level", value: `${titleCase(message.guild.verificationLevel)}`, inline: true },
+                { name: "AFK Timeout", value: (message.guild.afkChannel) ? `${moment.duration(message.guild.afkTimeout * 1000).asMinutes()} minute(s)` : "None", inline: true },
+                { name: "AFK Channel", value: (message.guild.afkChannel) ? `${message.guild.afkChannel.name}` : "None", inline: true },
+                { name: "Explicit Content Filter", value: `${titleCase(message.guild.explicitContentFilter)}`, inline: true },
+                { name: "Roles", value: `${roleCount}`, inline: true },
+                { name: "Server Owner", value: `[${message.guild.owner.user.tag}](https://discord.com/users/${message.guild.owner.user.id} "https://discord.com/users/${message.guild.owner.user.id}")`, inline: true },
+                { name: "Server ID", value: `${message.guild.id}`, inline: true }
+            )
+        message.channel.send(embed)
+    }
+}
 
 module.exports.help = {
-    name: "server-info",
-    aliases: ["serverinfo", "si"]
+    name: "serverinfo",
+    aliases: ["si"]
 }
