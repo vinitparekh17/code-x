a9febe6e2d2245c585422a1d55a3a7da66e5dba5
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-23 19:30:20 +0530

    Update qr.js

--- commands/qr.js
+++ commands/qr.js
@@ -7,13 +7,13 @@ module.exports.run = async(bot, message, args) => {
           // Input Checking
           if (!weblink) return message.channel.send('Please give me a link to turn into a QR-Code')
             const attachment = new MessageAttachment(link, 'qrcode.png');
-            const embed = new Discord.MessageEmbed()
+            const embed = new Discord.MessageEmbed() => {
               .setTitle(`QR-Code has been Generated!`)
               .attachFiles(attachment)
               .setColor('BLUE')
               .setImage('attachment://qrcode.png')
             message.channel.send(embed);
-            else { message.channel.send('Error! The input is not a valid link! Please make sure it is a valid link.\nWhen inputting the link please add `https://`!'); }
+           } else { message.channel.send('Error! The input is not a valid link! Please make sure it is a valid link.\nWhen inputting the link please add `https://`!'); }
        
   }
 
