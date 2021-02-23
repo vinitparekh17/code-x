20cd261cecab7bcc58eb655836b0437961118fb0
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-23 17:06:07 +0530

    Create qr.js

---
+++ commands/qr.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+module.exports.run = async(bot, message, args) {
+
+    let weblink = (args[0])
+    let link = `http://api.qrserver.com/v1/create-qr-code/?data=${weblink}&size=200x200`
+
+          // Input Checking
+          if (!weblink) return message.channel.send('Please give me a link to turn into a QR-Code')
+          if (require('is-url')(weblink)) {
+            const attachment = new MessageAttachment(link, 'qrcode.png');
+            const embed = new Discord.MessageEmbed()
+              .setTitle(`QR-Code has been Generated!`)
+              .attachFiles(attachment)
+              .setColor('BLUE')
+              .setImage('attachment://qrcode.png')
+            message.channel.send(embed);
+          } else { message.channel.send('Error! The input is not a valid link! Please make sure it is a valid link.\nWhen inputting the link please add `https://`!'); }
+       
+  }
+
+module.exports.help = {
+    name: "QR Genarator",
+    aliases: ["create-qr"]
+}
