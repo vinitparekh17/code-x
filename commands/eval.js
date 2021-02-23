65c4c6a7f52426879b3ac8f72c782470a686cdb7
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-23 22:37:37 +0530

    Create eval.js

---
+++ commands/eval.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+module.exports.run = async(bot, message, args) => {
+if (!message.author.id !== '467004231295959040') return;
+
+const code = args.join(" ");
+if(!code) return message.reply("leasee provide code to execute");
+
+try{
+const result = await eval(code);
+let output = result;
+if (typeof result !== "string") {
+  output = inspect(result);
+  }
+  
+  message.channel.send('output, { code: "js"}); } catch (error) {
+  console.log(error);
+  message.channel.send("Evaluated code is too long to display!");
+  }
+  }
+  
+  module.exports.help = {
+      name: "eval",
+      aliases: ["eval"]
+      }
