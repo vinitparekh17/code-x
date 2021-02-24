ef742d12a430d8566dac1dcc781b2ca350c366d4
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-24 15:42:51 +0530

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,9 +1,9 @@
 module.exports.run = async(bot, message, args) => {
 
-    let days = Math.floor(client.uptime / 86400000);
-    let hours = Math.floor(client.uptime / 3600000) % 24;
-    let minutes = Math.floor(client.uptime / 60000) % 60;
-    let seconds = Math.floor(client.uptime / 1000) % 60;
+    let days = Math.floor(bot.uptime / 86400000);
+    let hours = Math.floor(bot.uptime / 3600000) % 24;
+    let minutes = Math.floor(bot.uptime / 60000) % 60;
+    let seconds = Math.floor(bot.uptime / 1000) % 60;
 
     let uptimeE = new Discord.MessageEmbed()
     .setTitle("UPTIME")
