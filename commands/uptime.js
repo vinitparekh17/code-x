c2c9b625f77e09370cb689c76246fcc2cf022823
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-24 15:37:53 +0530

    Create uptime.js

---
+++ commands/uptime.js
@@ -0,0 +1,20 @@
+module.exports.run = async(bot, message, args) => {
+
+    let days = Math.floor(client.uptime / 86400000);
+    let hours = Math.floor(client.uptime / 3600000) % 24;
+    let minutes = Math.floor(client.uptime / 60000) % 60;
+    let seconds = Math.floor(client.uptime / 1000) % 60;
+
+    let uptimeE = new Discord.MessageEmbed()
+    .setTitle("UPTIME")
+    .setColor("RANDOM")
+    .setDescription(`\nDay(S) Online: ${days}\n\nHour(S) Online: ${hours}\n\nMinute(S) Online: ${minutes}\n\nSecond(S) Online: ${seconds}`)
+    .setFooter(`Requested by: ${message.author.username}`)
+    message.channel.send(uptimeE)
+    return;
+  }
+
+module.exports.help = {
+    name: "uptime"
+    aliases: ["uptime"]
+}
