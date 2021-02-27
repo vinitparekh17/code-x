629e0eb230824908d47215ea447c195ef95753f4
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-27 19:07:50 +0530

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 module.exports.run = async(bot, message, args) => {
  if (!message.author.id == '467004231295959040') return message.reply('This command is only for the developer.');
  if (!args[0]) return message.reply('You must specify code to eval.');
+ }
  try {
  let result = args.join(' ');
  if (args.join(' ') == 'process.exit()') return message.reply('Access Denied.');
