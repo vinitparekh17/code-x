eb85dcbb404d84271a96baa6a022406160666f16
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-27 19:02:38 +0530

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -3,10 +3,7 @@ module.exports.run = async(bot, message, args) => {
  if (!message.author.id == '467004231295959040') return message.reply('This command is only for the developer.');
  if (!args[0]) return message.reply('You must specify code to eval.');
  let result = args.join(' ');
- if (args.join(' ') == 'process.exit()') {
- return message.reply('Access Denied.');
- }
- try {
+ if (args.join(' ') == 'process.exit()') return message.reply('Access Denied.');
  let evaled = eval(result);
  if (!evaled.includes(bot.token)) {
  message.channel.send(`\`\`\`js\n${evaled}\`\`\``);
@@ -15,7 +12,6 @@ module.exports.run = async(bot, message, args) => {
  }
  } catch (error) {
  message.channel.send(`\`\`\`\n${error}\`\`\``);
- }
  }
  
  module.exports.help = {
