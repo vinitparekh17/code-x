94c31f2578617fdb3137f3b482cc59220648de3b
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-27 19:45:06 +0530

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,20 +1,19 @@
 const Discord = require('discord.js');
 module.exports.run = async(bot, message, args) => {
  if (!message.author.id == '467004231295959040') return message.reply('This command is only for the developer.');
- if (!args[0]) return message.reply('You must specify code to eval.');
- let result = args.join(' ');
- if (args.join(' ') == 'process.exit()') { 
- return message.reply('Access Denied.');
- } try {
- evaled = await eval(`(async () => {${result}})()`);
- if (typeof evaled !== "string")
+if(message.author.id !== '467004231295959040') return;
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string")
         evaled = require("util").inspect(evaled);
 
- message.channel.send((evaled), {code:"js"});
- } catch (err) {
- message.channel.send(`\`ERROR\` \`\`\`xl\n${(err)}\n\`\`\`);
- }
- }
+      message.channel.send((evaled), {code:"js"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${(err)}\n\`\`\``);
+    }
+  }
  
  module.exports.help = {
      name: "eval",
