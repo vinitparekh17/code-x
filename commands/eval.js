178dd6febe5a54c6000f003da25532710416b084
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-27 19:17:21 +0530

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js');
 module.exports.run = async(bot, message, args) => {
  if (!message.author.id == '467004231295959040') return message.reply('This command is only for the developer.');
  if (!args[0]) return message.reply('You must specify code to eval.');
- }
- try {
  let result = args.join(' ');
- if (args.join(' ') == 'process.exit()') return message.reply('Access Denied.');
+ if (args.join(' ') == 'process.exit()') { 
+ return message.reply('Access Denied.');
+ } try {
  let evaled = eval(result);
  if (!evaled.includes(bot.token)) {
  message.channel.send(`\`\`\`js\n${evaled}\`\`\``);
@@ -13,6 +13,7 @@ module.exports.run = async(bot, message, args) => {
  message.reply('The bot token is not available to the public.');
  } catch (err) {
  message.reply(`\`\`\`\n${err}\`\`\``);
+ }
  }
  
  module.exports.help = {
