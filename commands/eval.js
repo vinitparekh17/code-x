e02be130c8d9a823ed98999999fe448b065202d1
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-02-27 19:06:14 +0530

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -11,7 +11,7 @@ module.exports.run = async(bot, message, args) => {
  } else {
  message.reply('The bot token is not available to the public.');
  }
- } catch (error) {
+ } catch(error) {
  message.channel.send(`\`\`\`\n${error}\`\`\``);
  }
  
