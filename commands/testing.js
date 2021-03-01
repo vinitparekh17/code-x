e2093d58918a7ffb4f4b8a5af3626ec5302e8189
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-03-01 18:32:06 +0530

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-module.exports.run = async(client, message, args) => {
+module.exports.run = async(bot, message, args) => {
         const reactionEmojis = ["⬅️","➡️", "❌"];
         const StartEmbed = new Discord.MessageEmbed()
             .setAuthor("Help Embed", message.author.displayAvatarURL({ format: "png" }))
