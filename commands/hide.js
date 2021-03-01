465eae2ff2bc004280964db1b3fae80f2e056cea
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-03-01 11:57:43 +0530

    Create hide.js

---
+++ commands/hide.js
@@ -0,0 +1,21 @@
+module.exports.run = async (bot,message,args) => {
+  const Discord = require('discord.js')
+  if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send('You do not have enough permission');
+  if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send('Missing **"MANAGE CHANNEL"** permission');
+   message.channel.overwritePermissions([
+     {
+        id: message.guild.id,
+        deny : ['VIEW_CHANNEL'],
+     },
+    ],);
+  const hidembed = new Discord.MessageEmbed()
+  .setColor('#00FF00')
+  .setDescription(`<a:GG_true3:758699876858462258> <#${message.channel.id}> has been hidden!`)
+  message.channel.send(hidembed)
+
+  }
+
+module.exports.help = {
+    name: "hide",
+    aliases: ["hide"]
+  }
