cd15ebf12ae1d36e6b9f7eb851eaee94b7041abe
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-03-03 19:36:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -226,8 +226,9 @@ bot.on ("message"  , async message  =>  {
 
                 user.send(bEmbed)
 
-        })
-    }
+        }
+    };
+ 
      
 
      if(message.content.startsWith(prefix + "say" )){
