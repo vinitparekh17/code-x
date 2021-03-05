0906746c2010dbc392d2eaf46669624221fe3da8
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-03-05 21:18:04 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -5,7 +5,7 @@ message.channel.createInvite({ maxUses: 1, maxAge: 43200})
   .then(invite => message.author.send(`${invite.url}`))
   .catch(console.error);
 
-await message.channel.reply(`Invite link has been sent to your DMs`)
+message.channel.reply(`Invite link has been sent to your DMs`)
 
 } 
 
