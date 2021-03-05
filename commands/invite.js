57bc69821d34f2bae24d7caa10ccdff84c420b9f
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-03-05 21:11:10 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 module.exports.run = async(bot, message, args) => {
 
 message.channel.createInvite({ maxUses: 1, maxAge: 43200})
-  .then(invite => message.channel.send(`Genarating Invite link!`);
+  .then(invite => message.channel.send(`Genarating Invite link!`)
 message.edit(`${invite.url}`))
   .catch(console.error);
 
