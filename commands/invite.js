bd67911b182cc78bf0a7166990cc8e46c5a7ed50
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-03-05 21:15:39 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -2,9 +2,10 @@ const Discord = require('discord.js');
 module.exports.run = async(bot, message, args) => {
 
 message.channel.createInvite({ maxUses: 1, maxAge: 43200})
-  .then(invite => message.channel.send(`Genarating Invite link!`)
-message.edit(`${invite.url}`))
+  .then(invite => message.author.send(`${invite.url}`))
   .catch(console.error);
+
+await message.channel.reply(`Invite link has been sent to your DMs`)
 
 } 
 
