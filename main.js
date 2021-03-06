d9d200156615ea9ca766f774a9883afc7605cb36
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-03-06 19:11:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -244,9 +244,8 @@ ctx.drawImage(background, 0 , 0, canvas.width, canvas.height)
 ctx.strokeStyle = '#f2f2f2'
 ctx.strokeRect(0, 0 , canvas.width, canvas.height)
 var text1 = `${member.user.username}`
-var text2 = `${member.user.discriminator}`
-var text3 = `You're #${member.guild.memberCount} member!`
-var text4 = `${member.guild.name}`
+var text2 = `You're #${member.guild.memberCount} member!`
+var text3 = `${member.guild.name}`
 if(text1.length >= 14) {
   ctx.font = 'bold 100px Sans-Serif'
   ctx.fillStyle = '#f2f2f2'
@@ -258,17 +257,13 @@ else {
   ctx.fillText(text1, 720, canvas.height / 2 + 20)
 }
 
-ctx.font = 'bold 40px Sans-Serif'
+ctx.font = 'bold 60px Sans-Serif'
 ctx.fillStyle = '#f2f2f2'
-ctx.fillText(text2, 730, canvas.height / 2 + 58)
+ctx.fillText(text2, 750, canvas.height / 2 + 135)
 
 ctx.font = 'bold 60px Sans-Serif'
 ctx.fillStyle = '#f2f2f2'
-ctx.fillText(text3, 750, canvas.height / 2 + 135)
-
-ctx.font = 'bold 60px Sans-Serif'
-ctx.fillStyle = '#f2f2f2'
-ctx.fillText(text4, 700, canvas.height / 2 - 150)
+ctx.fillText(text3, 700, canvas.height / 2 - 150)
 
 ctx.beginPath()
 ctx.arc(315, canvas.height / 2, 250, 0, Math.PI * 2, true)
