f866e979bdd0b7e29a2d5f080ffab7b741c3f185
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-03-06 18:06:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -234,32 +234,57 @@ bot.on ("message"  , async message  =>  {
 
 })
 
-bot.on('guildMemberAdd' , member => 
-{
+const Canvas = require('canvas')
+bot.on('guildMemberAdd', async(member) => {
+if(!member.guild) return
+const canvas = Canvas.createCanvas(1772, 633)
+const ctx = canvas.getContext('2d')
+const background = await Canvas.loadImage('./welcome.png')
+ctx.drawImage(background, 0 , 0, canvas.width, canvas.height)
+ctx.strokeStyle = '#f2f2f2'
+ctx.strokeRect(0, 0 , canvas.width, canvas.height)
+var text1 = `${member.user.username}`
+var text2 = `${member.user.discriminator}`
+var text3 = `Member #${member.guild.memberCount}`
+var text4 = `${member.guild.name}`
+if(text1.length >= 14) {
+  ctx.font = 'bold 100px Sans-Serif'
+  ctx.fillStyle = '#f2f2f2'
+  ctx.fillText(text1, 720, canvas.height / 2 +20)
+}
+else {
+  ctx.font = 'bold 150px Sans-Serif'
+  ctx.fillStyle = '#f2f2f2' 
+  ctx.fillText(text1, 720, canvas.height / 2 + 20)
+}
 
+ctx.font = 'bold 40px Sans-Serif'
+ctx.fillStyle = '#f2f2f2'
+ctx.fillText(text2, 730, canvas.height / 2 + 58)
 
+ctx.font = 'bold 60px Sans-Serif'
+ctx.fillStyle = '#f2f2f2'
+ctx.fillText(text3, 750, canvas.height / 2 + 135)
 
- wmbd = new Discord.MessageEmbed()
- .setTitle(`${member.user.username} has joined the server`)
- .setColor('RANDOM')
- .setDescription(`✫▬▬▬✫▬▬▬✫▬▬▬✫▬▬▬✫▬▬▬
+ctx.font = 'bold 60px Sans-Serif'
+ctx.fillStyle = '#f2f2f2'
+ctx.fillText(text4, 700, canvas.height / 2 - 150)
 
- <a:hypesquad:779012310504964126>  Welcome <@${member.user.id}> <a:hypesquad:779012310504964126> 
+ctx.beginPath()
+ctx.arc(315, canvas.height / 2, 250, 0, Math.PI * 2, true)
+ctx.closePath()
+ctx.clip()
 
-<a:GG_true3:751658895247802429>  Read our server rules from <#751657642660855879>
-
-<a:GG_true3:751658895247802429>  Take your roles from <#726836834541240360>
-
-<a:GG_true3:751658895247802429>  You're ${bot.users.cache.size} member of this Server! <a:donald_clap:750700622830895204> 
-
-✫▬▬▬✫▬▬▬✫▬▬▬✫▬▬▬✫▬▬▬`)
- .setTimestamp()
- .setImage('https://cdn.discordapp.com/attachments/652183126055059469/672054114993176586/welcome-dribbble.gif')
- 
-
-bot.guilds.cache.get('724858940252487720').channels.cache.get('724859956561444884').send(wmbd)
-member.send(wmbd)
-
+const avatar = await Canvas.loadImage(member.user.displayAvatarURL({format: 'jpg'}))
+ctx.drawImage(avatar, 65, canvas.height / 2 - 250, 500, 500)
+const attachment = new DiscordJS.MessageAttachment(canvas.toBuffer(), 'welcome-image.png')
+const welcome = new DiscordJS.MessageEmbed()
+  .setColor('RED')
+  .setTimestamp()
+  .setImage('attachment://welcome-image.png')
+  .attachFiles(attachment)
+  const channel = member.guild.channels.cache.find(ch => ch.id === 'ID YOUR CHANNEL')
+  channel.send(welcome)
 });
 
 
