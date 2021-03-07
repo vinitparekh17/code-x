6c2d2041c4878535ef429bd825edd454e76266c3
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-03-07 10:26:46 +0530

    Update trigger.js

--- commands/trigger.js
+++ commands/trigger.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const { Canvas } = require('canvacord');
 module.exports.run = async(bot, message, args) => {
-    const user = message.members.first() || message.author;
+    const user = message.mentions.members.first() || message.author;
     const avatar = user.displayAvatarURL();
     const image = await Canvas.trigger(avatar);
     message.channel.send(
