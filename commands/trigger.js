87fe01fb4f6669887f9fece4b645a17ab591dd49
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-03-07 11:08:40 +0530

    Update trigger.js

--- commands/trigger.js
+++ commands/trigger.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const { Canvas } = require('canvacord');
 module.exports.run = async(bot, message, args) => {
     const user = message.mentions.members.first() || message.author;
-    const image = await Canvas.wanted(user.displayAvatarURL {format: 'png'});
+    const image = await Canvas.wanted(user.displayAvatarURL({format: 'png'}));
     message.channel.send({
     files: [{
         attachment: image,
