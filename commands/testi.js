12f235f01fcc6295b260d73204ef7e0e70277f66
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-03-14 13:56:50 +0530

    Add files via upload

---
+++ commands/testi.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const recon = require('reconlx');
+const ReactionPages = recon.ReactionPages;
+
+module.exports.run = async(bot, message, args) => {
+    const embed1 = new Discord.MessageEmbed()
+    .setTitle(`The Guardian Bot Help & Support`)
+    .setColor(#00FFDB)
+    .setFooter(`Pages 1/2`)
+    
+    const embed2 = new Discord.MessageEmbed()
+    .setTitle(`The Guardian Bot Help & Support`)
+    .setColor(#00FFDB)
+    .setFooter(`Pages 2/2`)
+    
+    const pages = [embed1. emved2];
+    const emojis = ['➡️' , '⬅️'];
+    ReactionPages = (message, pages, reaction, true, emojis, 10*1000 )
+}
+
+module.exports.help = {
+    name: "testbd"
+    aliases: ["testy"]
+}
