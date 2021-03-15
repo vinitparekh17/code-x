bf8aae3627cc0745765898f863ba771aefa503b9
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-03-15 15:56:45 +0530

    Create chatbot.js

---
+++ commands/chatbot.js
@@ -0,0 +1,11 @@
+const { chatBot } = require('reconlx') 
+
+module.exports.run = async(bot, message, args) => {
+        chatBot(message, args.join(" "))
+    }
+}
+
+module.exports.help = {
+    name: "chatting",
+    aliases: ["<@778284711428096000>"]
+}
