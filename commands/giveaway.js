e21c2297c66db1f7f56e3e0696e50c480bb0dafc
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-03-15 13:49:04 +0530

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -44,7 +44,6 @@ module.exports.run = async(bot, message, args) => {
         })
         message.channel.send(`Giveaway is starting in ${channel}`)
     }
-}
 
 module.exports.help = {
     name: "Giveaway",
