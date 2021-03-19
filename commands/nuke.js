f12f135d2dda451ddc8044d8571f54e1c3e9eae5
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-03-19 21:11:06 +0530

    Update nuke.js

--- commands/nuke.js
+++ commands/nuke.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 module.exports.run = async(bot, message, args) => {
 
 if(!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`You don't have enough permission to use this command!`)
-let filter = m => m.author.id
+let filter = m => m.author.id === message.author.id
 
 message.reply(`Are you sure? \nReply with \`Yes\` or \`No\``)
         message.channel.awaitMessages(filter, {
