f53c86068eba9929f67b09f4e31f07f711bb34c5
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-03-21 10:36:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -117,6 +117,71 @@ bot.on ("message"  , async message  =>  {
 
 
      }
+const { GiveawaysManager } = require('discord-giveaways');
+// Starts updating currents giveaways
+const manager = new GiveawaysManager(bot, {
+    storage: './giveaways.json',
+    updateCountdownEvery: 10000,
+    default: {
+        botsCanWin: false,
+        exemptPermissions: ['MANAGE_MESSAGES', 'ADMINISTRATOR'],
+        embedColor: '#FF0000',
+        reaction: '🎉'
+    }
+});
+// We now have a giveawaysManager property to access the manager everywhere!
+bot.giveawaysManager = manager;
+
+bot.on('message', (message) => {
+
+    if (message.content.startsWith(prefix + "gstart")){
+        // g!start-giveaway 2d 1 Awesome prize!
+        // will create a giveaway with a duration of two days, with one winner and the prize will be "Awesome prize!"
+
+        bot.giveawaysManager.start(message.channel, {
+            time: ms(args[0]),
+            prize: args.slice(2).join(' '),
+            winnerCount: parseInt(args[1])
+        }).then(() => {
+            message.channel.send('Giveaway started in the current channel!');
+        });
+        // And the giveaway has started!
+    }
+
+    if(message.content.startsWith (prefix + "reroll")) {
+        const messageID = args[0];
+        bot.giveawaysManager.reroll(messageID).then(() => {
+            message.channel.send('Success! Giveaway rerolled!');
+        }).catch(() => {
+            message.channel.send('No giveaway found for ' + messageID + ', please check and try again');
+        });
+    }
+
+    if(message.content.startsWith(prefix + "edit")){
+        const messageID = args[0];
+        bot.giveawaysManager.edit(messageID, {
+            newWinnerCount: 3,
+            newPrize: 'New Prize!',
+            addTime: 5000
+        }).then(() => {
+            // here, we can calculate the time after which we are sure that the lib will update the giveaway
+            const numberOfSecondsMax = bot.giveawaysManager.options.updateCountdownEvery / 1000;
+            message.channel.send('Success! Giveaway will updated in less than ' + numberOfSecondsMax + ' seconds.');
+        }).catch(() => {
+            message.channel.send('No giveaway found for ' + messageID + ', please check and try again');
+        });
+    }
+
+    if (message.content.startsWith(prefix + "delete")){
+        const messageID = args[0];
+        bot.giveawaysManager.delete(messageID).then(() => {
+            message.channel.send('Success! Giveaway deleted!');
+        }).catch(() => {
+            message.channel.send('No giveaway found for ' + messageID + ', please check and try again');
+        });
+    }
+
+});
 
 
     if (message.content.startsWith(prefix + "members")){
