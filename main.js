ae8b89fd21fa892af51dc9bba6acc68cbb9f6ead
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-03-25 18:49:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -60,9 +60,23 @@ bot.user.setStatus('dnd')
 
 })
 
+bot.on('message', message => {
+    const fetch = require(node-fetch);
+    const channelid = '823778163942227968' // Channel ID
 
+    if(message.channel.id === channelid) {
 
-bot.on ("message"  , async message  =>  {
+        if(message.author.bot) return
+
+        fetch(
+            `https://api.monkedev.com/fun/chat?msg=${encodeURIComponent(message)}&uid=000` // API For ChatBot
+        )
+            .then((res) => res.json())
+            .then(async (json) => {
+                return message.channel.send(`> ${message}\n${json.response}`); // User Message+Reply
+            });
+    }
+})bot.on ("message"  , async message  =>  {
     const args = message.content.slice(prefix.length).trim().split(' ');
     const cmd = args.shift().toLowerCase();
     let command
