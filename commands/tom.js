7f15b18e23d3d79975b35f5f324c41e4ea4f7865
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-04-01 18:02:30 +0530

    Update tom.js

--- commands/tom.js
+++ commands/tom.js
@@ -1,5 +1,5 @@
 const { MessageAttachment } = require('discord.js')
-module.exports.run = async run(bot, message, args) => {
+module.exports.run = async(bot, message, args) => {
     user = await this.verifyUser(message, user, true);
 
     const img = await this.bot.img.tom(user.displayAvatarURL({ size: 256, format: "png" }));
