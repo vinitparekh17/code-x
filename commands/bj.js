e873244c493d523cd6a050ce469f1f5471cee2f5
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-04-01 17:37:40 +0530

    Create bj.js

---
+++ commands/bj.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports.run = async(bot, message, args) => {
+const { url } = await fetch("https://nekos.life/api/v2/img/bj")
+      .then((res) => res.json());
+
+    const embed = new Discord.MessageEmbed()
+      .setTitle(`Random bj images has been generated!`)
+      .setDescription(`[click here to get link](${url})`)
+      .setColor(`#FF00FF`)
+      .setImage(url)
+      .setFooter(`Requested by: ${message.author.tag} | Powered by nekos.life`, message.author.displayAvatarURL({ size: 32 }));
+
+    message.channel.send(embed)
+}
+
+module.exports.help = {
+    name: "bj",
+    aliases: ["bj"]
+}
