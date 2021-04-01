24a03ed90a1246711853e7e6ec449138918a3845
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-04-01 15:19:40 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
-module.exports.run = async (client, message, args) => {
+module.exports.run = async (bot, message, args) => {
     const m = await message.channel.send("Ping?")
-       m.edit(`🏓 Pong!\nLatency is ${Math.floor(message.createdTimestamp - message.createdTimestamp)}ms\nAPI Latency is ${Math.round(client.ping)}ms`)
+       m.edit(`>>>🏓 Pong!\nLatency is \`${Math.floor(message.createdTimestamp - message.createdTimestamp)}ms\`\nAPI Latency is \`${Math.round(bot.ping)}ms\``)
 
 }
 
