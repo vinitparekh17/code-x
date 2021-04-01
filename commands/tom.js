c0a19316b89173eeb5a43c304aaf9d538dba36fa
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-04-01 18:00:32 +0530

    Create tom.js

---
+++ commands/tom.js
@@ -0,0 +1,13 @@
+const { MessageAttachment } = require('discord.js')
+module.exports.run = async run(bot, message, args) => {
+    user = await this.verifyUser(message, user, true);
+
+    const img = await this.bot.img.tom(user.displayAvatarURL({ size: 256, format: "png" }));
+
+     message.channel.send(new MessageAttachment(img, "tom.png"));
+  }
+
+module.exports.help = {
+    name: "Tom",
+    aliases: ["Tom"]
+}
