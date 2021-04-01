bf9b4885cde1777fbf0e035a5fdc6e4059d420a7
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-04-01 15:27:28 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js')
 module.exports.run = async (bot, message, args) => {
     const m = await message.channel.send("Ping?")
     const embed = new Discord.MessageEmbed()
