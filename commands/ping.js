e66c409f2c4d210c600c397827972b58f9405f60
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-04-01 15:25:02 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,11 @@
 module.exports.run = async (bot, message, args) => {
     const m = await message.channel.send("Ping?")
-       m.edit(`>>>🏓 Pong!\nLatency is \`${Math.floor(message.createdTimestamp - message.createdTimestamp)}ms\`\nAPI Latency is \`${Math.round(bot.ping)}ms\``)
+    const embed = new Discord.MessageEmbed()
+    .setTitle(`Ping...`)
+    .setDescription(`>>> 🏓 Pong!\nLatency is \`${Math.floor(message.createdTimestamp - message.createdTimestamp)}ms\``)
+    .setColor(`RANDOM`)
+    .setTimestamp()
+       m.edit(embed)
 
 }
 
