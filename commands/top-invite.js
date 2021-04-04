d2140cbc2fd2f039c1f7ab3a6dcc062dcf2819d0
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-04-04 12:45:36 +0530

    Update top-invite.js

--- commands/top-invite.js
+++ commands/top-invite.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 module.exports.run = async(bot, message, args) => {
-    const invites = await msg.guild.fetchInvites();
+    const invites = await message.guild.fetchInvites();
     const topTen = invites.filter((inv) => inv.uses > 0).sort((a, b) => b.uses - a.uses).first(10);
 
     if(topTen.length === 0) return msg.send("There are no invites, or none of them have been used!");
 
     const embed = new Discord.MessageEmbed()
-      .setTitle(`Top Invites in ${msg.guild.name}`)
-      .setAuthor(msg.guild.name, msg.guild.iconURL())
+      .setTitle(`Top Invites in ${message.guild.name}`)
+      .setAuthor(message.guild.name, message.guild.iconURL())
       .setDescription(topTen.map((inv) => `• **${inv.inviter.username}**'s invite **${inv.code}** has **${inv.uses.toLocaleString()}** uses.`).join("\n"))
        message.channel.send(embed)
 }
