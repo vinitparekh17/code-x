0a05824d87b76ccaace2af176a0f090fe187bc44
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-04-04 14:19:15 +0530

    Update ui.js

--- commands/ui.js
+++ commands/ui.js
@@ -3,9 +3,9 @@ module.exports.run = async(bot, message, args) => {
    const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
 
 this.statuses = {
-      online: "<:online:473263910045351957> Online",
-      idle: "<:idle:473264190346493964> Idle",
-      dnd: "<:dnd:473264076852559873> Do Not Disturb",
+      online: "<:online:828186993007525899> Online",
+      idle: "<:idle:828187183331672094>  Idle",
+      dnd: "<:dnd:828187347043745842>  Do Not Disturb",
       offline: "Offline"
     }
 
@@ -14,8 +14,8 @@ this.statuses = {
       .setThumbnail(member.user.displayAvatarURL({ size: 512 }))
       .addField("❯ Name", member.user.tag, true)
       .addField("❯ ID", member.id, true)
-      .addField("❯ Discord Join Date", `new Date(user.createdTimestamp).toLocaleDateString()`)
-      .addField("❯ Server Join Date", `new Date(user.createdTimestamp).toLocaleDateString()`)
+      .addField("❯ Discord Join Date", new Date(user.createdTimestamp).toLocaleDateString())
+      .addField("❯ Server Join Date", new Date(user.createdTimestamp).toLocaleDateString())
       .addField("❯ Status", this.statuses[member.presence.status], true)
       .addField("❯ Bot", member.user.bot ? "Yes" : "No", true)
       .addField("❯ Highest Role", member.roles.cache.size > 1 ? member.roles.highest.name : "None", true)
