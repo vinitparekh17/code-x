4030f102b0e4b0da11ddef1e277cbe305518dc0f
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-04-04 15:20:59 +0530

    Update steal.js

--- commands/steal.js
+++ commands/steal.js
@@ -5,12 +5,8 @@ module.exports.run = async(message, [emoji, name]) => {
     if(e) return this.create(message, e.url, name, "Something went wrong.");
     const url = `https://cdn.discordapp.com/emojis/${emoji}.png`;
     return this.create(message, url, name, "Couldn't create emoji, make sure the ID is valid.");
-  }
-  
-  message.reply(message, url, name, err = "Something went wrong, make sure the URL is valid and returns an image.") {
+
     const emoji = await message.guild.emojis.create(url, name).catch(() => {
-      throw err;
-    });
     message.channel.send(`Done! created new emoji ${emoji.name} ${emoji.toString()}`);
   }
   
