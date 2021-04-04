34487cb761967c193e282694b366e39f811162f7
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-04-04 11:51:11 +0530

    Update win.js

--- commands/win.js
+++ commands/win.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js')
 module.exports.run = async(bot,message,args) => {
-const a = args.join(" ").split(",")
+const args[0] = a
+const args[1] = b
 if (args[0] === null) return message.channel.send('Please give winners ');
 
 const embed = new Discord.MessageEmbed()
 
 .setTitle("Each winner getss 50rs each")
-.addField(`<a:Minigame_Crown:795920833645903872> Winners <a:Minigame_Crown:795920833645903872>\nNo. 1 : args[0] \nNo. 2 : args[1]`) 
+.addField(`<a:Minigame_Crown:795920833645903872> Winners <a:Minigame_Crown:795920833645903872>\nNo. 1 : ${a} \nNo. 2 : ${b}`) 
 
 .setColor("#00ffff")   
 .setFooter(bot.user.username, bot.user.displayAvatarURL());
