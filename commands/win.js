2df31de3acf98326949240252143c8cd10fac2e6
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-04-04 11:01:22 +0530

    Update win.js

--- commands/win.js
+++ commands/win.js
@@ -6,7 +6,7 @@ if (args[0] === null) return message.channel.send('Please give winners ');
 const embed = new Discord.MessageEmbed()
 
 .setTitle("Each winner getss 50rs each")
-.addField('<a:Minigame_Crown:795920833645903872> Winners <a:Minigame_Crown:795920833645903872>', \n Rank 1 : args[0]\nRank 2 : args[1]') 
+.addField('<a:Minigame_Crown:795920833645903872> Winners <a:Minigame_Crown:795920833645903872>', \nNo. 1 : args[0]\nNo. 2 : args[1]') 
 
 .setColor("#00ffff")   
 .setFooter(message.bot.user.username, message.bot.user.displayAvatarURL());
