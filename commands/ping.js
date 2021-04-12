dc67bec63dcada5ff2ed09db00cfcf3d6492e789
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-04-12 13:37:09 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,11 +5,12 @@ module.exports.run = async (bot, message, args) => {
     const m = await message.channel.send(cembed)
     const embed = new Discord.MessageEmbed()
     .setTitle(`Ping...`)
-    .addField(":green: Letancy" , m.createdTimestamp() - message.createdTimestamp())
-    .addField(":green: API Letancy" , ${Math.round(bot.ws.ping)})
+    .addField("<a:green:829283875040657419> Letancy" , `\`\`\`py\n${Date.now() - message.createdTimestamp} ms\`\`\``)
+    .addField("<a:green:829283875040657419> API Letancy" , `\`\`\`py\n${Math.round(bot.ws.ping)} ms\`\`\``)
     .setThumbnail(`https://c.tenor.com/CwBokC9f1xMAAAAM/king-barbarian.gif`)
     .setColor(`#D2FF00`)
-    .setTimestamp()
+    .setFooter("Dev • $VINIT$#1742" , `https://cdn.discordapp.com/avatars/467004231295959040/72241ee3123c736bf914f9d05a5a6491.webp
+`)
        m.edit(embed)
 
 }
