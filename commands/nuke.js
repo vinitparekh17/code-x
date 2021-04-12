77bece79aef2bf0f3e156fb70711ceb8bc4583f2
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-04-12 20:53:38 +0530

    Update nuke.js

--- commands/nuke.js
+++ commands/nuke.js
@@ -18,7 +18,7 @@ message.channel.clone().then((ch) => {
     ch.setPosition(message.channel.position);
     message.channel.delete();
 
-ch.send(``\message.author.tag\` nuked this channel!`, {
+ch.send(` `\message.author.tag\` nuked this channel! `, {
     files: [{
         attachment: 'https://cdn.discordapp.com/attachments/830741522571329536/830741527503306770/dribbblepost_03.gif',
         name: 'file.gif'
