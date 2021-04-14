5b25a02f0ea050584a51ca99368b764e1b3377df
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-04-14 16:37:37 +0530

    Add files via upload

---
+++ commands/emsg.js
@@ -0,0 +1,49 @@
+module.exports.run = async(bot, message, args) => {
+  
+  
+  
+let emojis = msg.guild.emojis.cache.filter(emoji => emoji.animated);
+const ids = (emojis.map(e => '<'+'a'+':'+ e.name +':'+e.id+'>'
+));
+const names = (emojis.map(e => ':'
++ e.name + ':'));
+
+
+
+let keys = names;
+let values = ids;
+let result =  Object.assign.apply({}, keys.map( (v, i) => ( {[v]: values[i]} ) ) );
+
+
+
+let sayMessage = args.slice(1).join(" ");
+let str = sayMessage;
+let arr = result;
+let new_str = str;
+
+for (var key in arr) {
+    if (!arr.hasOwnProperty(key))
+    {
+        continue;
+    }
+
+  new_str = new_str.split(key).join(arr[key])
+
+  
+}
+    
+ 
+ 
+let channel = msg.mentions.channels.first();
+if (!channel) return msg.reply('Please provide a channel');
+if (!sayMessage) return msg.reply
+('Please provide a message');
+ 
+ channel.send(new_str)
+ 
+  }
+  
+  module.exports.help = {
+      name: "emsg",
+      aliases: ["emsg"]
+  }
