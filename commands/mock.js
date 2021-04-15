cfaf79bb17b86a4c26e46a001e75a3a2cf0668ea
vinitsoni17 <77158947+vinitsoni17@users.noreply.github.com>
2021-04-15 18:21:50 +0530

    Update mock.js

--- commands/mock.js
+++ commands/mock.js
@@ -4,7 +4,7 @@ const randomizeCase = word => word.split('').map(c => Math.random() > 0.5 ? c.to
 module.exports.run = (bot, message, args) => {
 	if (args.length < 1) return message.channel.send("Please provide some text to Mock")
 
-	let mockEmbed = new Discord.RichEmbed()
+	let mockEmbed = new Discord.MessageEmbed()
 		.setColor("#00ff00")
 		.setDescription(args.map(randomizeCase))
 		.setImage("https://cdn.discordapp.com/attachments/424889733043191810/425242569325150208/mock.jpg")
